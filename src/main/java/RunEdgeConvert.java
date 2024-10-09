9271801d2c559cdd72bd65d484fe2af882cb3d79
al2231 <112445369+al2231@users.noreply.github.com>
2024-10-09 17:46:21 -0400

    RunEdgeConvert.java logging

--- src/main/java/RunEdgeConvert.java
+++ src/main/java/RunEdgeConvert.java
@@ -1,5 +1,11 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class RunEdgeConvert {
+   private final Logger logger = LogManager.getLogger(RunEdgeConvert.class.getName());
+   
    public static void main(String[] args) {
       EdgeConvertGUI edge = new EdgeConvertGUI();
+      logger.info("RunEdgeConvert main function ran, EdgeConvertGUI called");
    }
 }
