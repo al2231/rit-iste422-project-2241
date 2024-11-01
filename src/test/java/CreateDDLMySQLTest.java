9b34e6097257b9244b11c66cdbabca55d5e76e61
al2231 <student@localhost.localdomain>
2024-11-01 12:44:56 -0400

    testing content

--- src/test/java/CreateDDLMySQLTest.java
+++ src/test/java/CreateDDLMySQLTest.java
@@ -1,5 +1,4 @@
 import static org.hamcrest.CoreMatchers.*;
-import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -8,75 +7,114 @@ import org.junit.Test;
 
 public class CreateDDLMySQLTest {
 	CreateDDLMySQL testObj;
+    EdgeTable[] dummyTables;
+    EdgeField[] dummyFields;
 
 	@Before
 	public void setUp() throws Exception {
+
+        EdgeTable table1 = new EdgeTable("1|Table");
+        EdgeField field1 = new EdgeField("1|Field|0|255|true|false");
+        
+        // table1.addNativeField(1);
+
+        dummyTables = new EdgeTable[] { table1 };
+        dummyFields = new EdgeField[] { field1 };
+
         //regular constructor
-		testObj = new CreateDDLMySQL();
+		testObj = new CreateDDLMySQL(dummyTables, dummyFields);
         assertThat(testObj, notNullValue());
     }
 
     @Test
 	public void defaultConstructor() {
-        CreateDDLMySQL test = new CreateDDLMySQL();
-        assertThat(testObj, notNullValue());
-        assertThat("dep", testObj.getClass(), is(10.0));
+		CreateDDLMySQL test = new CreateDDLMySQL();
+
+        assertThat(test, notNullValue());
     }
 
 	@Test
 	public void generateDatabaseNameisNull_thenReturnEmptyString() {
-        assertThat(testObj, notNullValue());
-        assertThat("dep", testObj.getClass(), is(10.0));
+        testObj.generateDatabaseName();
+        assertThat("Empty database name when user cancels input", 
+            testObj.getDatabaseName(), is(""));
     }
 
     @Test
 	public void generateDatabaseNameisEntered_thenReturnDatabaseName() {
-        assertThat(testObj, notNullValue());
-        assertThat("dep", testObj.getClass(), is(10.0));
+        String db = "test";
+        testObj.generateDatabaseName();
+        assertThat("dep", testObj.getDatabaseName(), is(db));
     }
 
     @Test
 	public void createDDLisSuccessful() {
+
+        testObj.databaseName = "test";
         //non-empty database name
         //SQL string contains CREATE DATABASE {dbName}; and USE {dbName};
+        testObj.createDDL();
+        String ddl = testObj.getSQLString();
         assertThat("non-empty database name", testObj.getDatabaseName(), notNullValue());
-        assertThat("string contains CREATE DATABASE and USE;", testObj.getSQLString(), is(10.0));
+        assertThat("string contains CREATE DATABASE and USE;", ddl, containsString("CREATE DATABASE test;"));
+        assertThat("string contains CREATE DATABASE and USE;", ddl, containsString("USE test;"));
+
     }
 
     @Test
 	public void createDDLgivenNoPrimaryOrForeignKeys_thenNoKeyConstraints() {
-        assertThat(testObj, notNullValue());
-        assertThat("dep", testObj.getClass(), is(10.0));
+
+        testObj.databaseName = "noKeys";
+        testObj.createDDL();
+        String ddl = testObj.getSQLString();
+        assertThat("should not contain primary key constraint", ddl, not(containsString("PRIMARY KEY")));
+        assertThat("should not contain foreign key constraint", ddl, not(containsString("FOREIGN KEY")));
+
     }
 
     @Test
 	public void createDDLgivenSingleFieldAsPrimaryKey_thenIncludesPrimaryKeyConstraint() {
-        assertThat(testObj, notNullValue());
-        assertThat("dep", testObj.getClass(), is(10.0));
+
+        testObj.createDDL();
+        String ddl = testObj.getSQLString();
+        assertThat("should contain primary key constraint", ddl, containsString("PRIMARY KEY"));
+
     }
 
     @Test
 	public void createDDLgivenTablewithForeignKey_thenIncludesForeignKeyConstraint() {
-        assertThat(testObj, notNullValue());
-        assertThat("dep", testObj.getClass(), is(10.0));
+
+        testObj.createDDL();
+        String ddl = testObj.getSQLString();
+        assertThat("should contain foreign key constraint", ddl, containsString("FOREIGN KEY"));
+
     }
 
     @Test
 	public void createDDLgivenFieldOfVarcharType_thenIncludesVarchar() {
-        assertThat(testObj, notNullValue());
-        assertThat("dep", testObj.getClass(), is(10.0));
+
+        testObj.createDDL();
+        String ddl = testObj.getSQLString();
+        assertThat("should contain VARCHAR type for field", ddl, containsString("VARCHAR"));
+
     }
 
     @Test
 	public void createDDLgivenFieldOfNotNull_thenIncludesNotNullConstraint() {
-        assertThat(testObj, notNullValue());
-        assertThat("dep", testObj.getClass(), is(10.0));
+
+        testObj.createDDL();
+        String ddl = testObj.getSQLString();
+        assertThat("should contain NOT NULL for field", ddl, containsString("NOT NULL"));
+
     }
 
     @Test
 	public void createDDLgivenDBNameIsEmpty_thenReadSuccessFalseAndNoSQLDDL() {
-        assertThat(testObj, notNullValue());
-        assertThat("dep", testObj.getClass(), is(10.0));
+
+        testObj.databaseName = "";
+        String ddl = testObj.getSQLString();
+        assertTrue("no SQL DDL generated when db empty", ddl.isEmpty());
+        // assertFalse("read succes should be False", EdgeConvertGUI.isReadSuccess());
     }
 
     @Test
